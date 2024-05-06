19966ae2264e4b81e396d3a980d8d79d944134c4
pedrooalves24 <pedrovoalves2004@gmail.com>
2024-05-06 20:26:45 +0100

    first committ

---
+++ src/App.vue
@@ -0,0 +1,181 @@
+<template>
+  <div class="app" >
+    <Carousel class="carousel" :current-slide="currentSlide">
+      <Slide v-for="index in 4" :key="index">
+        <div v-show="currentSlide === index" class="slide-info" v-if="index === 1">
+          <p>What's my favorite line of the Shrek movie?</p>
+        </div>
+
+        <div v-show="currentSlide === index" class="slide-info" v-else-if="index === 2">
+          <p>Choose your favorite date</p>
+          <input type="date" v-model="selectedDate" class="date-input">
+        </div>
+
+        <div v-show="currentSlide === index" class="slide-info" v-else-if="index === 3">
+          <div class="flexbox-container">
+            <div class="flex-1">Imagine creating layouts with floats and positioning...</div>
+            <div class="flex-2">Why is the Shrek script here?</div> 
+            <div class="flex-3">I was hoping there would be free bitcoin :(</div>
+            <div class="flex-4">SHREK 1 > ALL THE OTHER SHRECK'S</div>
+            <div class="flex-5"></div>
+          </div>
+        </div>
+
+        <div v-show="currentSlide === index" class="slide-info" v-else-if="index === 4">
+          <img :src="require(`./assets/challenge.png`)" alt="">
+        </div>
+      </Slide>
+    </Carousel>
+    <!-- Something outside the carousel -->
+    <div v-if="currentSlide === 1" class="answer1">
+      <p>"[♪ All-Star By Smash Mouth Playing]"</p>
+    </div>
+    <div v-else-if="currentSlide === 2" class="answer2">
+      <p>Selected Date <br> {{ selectedDate }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref , provide} from "vue";
+import Carousel from './components/Carousel.vue';
+import Slide from './components/Slide.vue';
+
+export default {
+  name: 'App',
+  components: { Carousel, Slide },
+  setup(){
+    const selectedDate = ref('');
+    const currentSlide = ref(1);
+
+    provide("currentSlide", currentSlide);
+
+    return { selectedDate , currentSlide};
+  }
+};
+
+</script>
+
+<style lang="scss" scoped>
+.carousel {
+  display: flex;
+  position: relative;
+  width: 1000px;
+  height: 500px;
+  justify-content: center;
+  top: 100px;
+  margin: 0 auto;
+  border-radius: 50px; /* Rounded corners */
+  background-color: #F9F9F9;
+
+  .slide-info {
+    display: flex;
+    position: absolute;
+    top: 0; 
+    left: 0; 
+    width: 100%; 
+    height: 100%; 
+    justify-content: center;
+    align-items: center;
+    text-align: center; /* Center text horizontally */
+
+    img {
+      max-width: 100%; /* Make sure the image fits within the slide */
+      max-height: 100%; /* Make sure the image fits within the slide */
+      object-fit: cover; /* Cover the entire area of the image container */
+    }
+
+    .date-input {
+    /* Estilos gerais */
+      z-index: 10;
+      width: 20%;
+      padding: 0.5rem;
+      font-size: 1rem;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+      background-color: #fff; /* Cor de fundo */
+      color: #333; /* Cor do texto */
+    }
+
+    .date-input:hover,
+    .date-input:focus {
+      border-color: #007bff; /* Cor da borda quando o campo está em foco */
+      outline: none; /* Remove a borda de foco padrão */
+    }
+
+    .date-input:disabled {
+      background-color: #eee; /* Cor de fundo quando o campo está desativado */
+      color: #999; /* Cor do texto quando o campo está desativado */
+    }
+
+    .flexbox-container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      text-align: center; /* Center text horizontally */
+    } 
+
+    .flex-1 {
+      font-family: "Montserrat", sans-serif;
+      font-size: 50px;
+      font-weight: bold;
+      border-radius: 5px;
+      margin-bottom: 20px;
+      justify-content: flex-start;
+    }
+
+    .flex-2 {
+      font-family: "Montserrat", sans-serif;
+      font-size: 35px;
+      border-radius: 5px;
+      margin-left: 60px;
+      align-self: flex-start; /* Align to the start of the flex container */
+    }
+
+    .flex-3 {
+      font-family: "Montserrat", sans-serif;
+      font-size: 20px;
+      border-radius: 5px;
+      margin-left: 100px;
+      margin-bottom: 20px;
+      flex-shrink: 7; /* Allow shrinking to fit content */
+      align-self: flex-start; /* Align to the start of the flex container */
+    }
+    .flex-4 {
+      font-family: "Montserrat", sans-serif;
+      font-size: 24px;
+      font-weight: bold;
+      border-radius: 5px;
+      align-self: center;
+    }
+    .flex-5 {
+      font-family: "Montserrat", sans-serif;
+      font-size: 24px;
+      border-radius: 5px;
+    }
+  }
+
+  .slide-info p,
+  .slide-info label {
+    font-family: 'Montserrat', sans-serif; /* Define a fonte */
+    font-size: 50px; /* Define o tamanho da fonte */
+    font-weight: bold;
+  }
+}
+
+  .answer1,
+  .answer2 {
+    position: absolute;
+    top: 75%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 10; /* Ensure the answer appears above the carousel */
+  }
+
+  .answer1 p,
+  .answer2 p {
+    font-family: "Montserrat", sans-serif;
+    font-size: 24px;
+    font-weight: bold;
+  }
+</style>
---
+++ src/components/Carousel.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class="carousel">
+    <slot :currentSlide = "currentSlide"/>  
+    <!--NAVIGATION-->
+    <div class="navigation">
+        <div class="toggle-page left">
+            <i @click="prevSlide" class="fas fa-chevron-left"></i>
+        </div>
+        <div class="toggle-page right">
+            <i @click="nextSlide" class="fas fa-chevron-right"></i>
+        </div>
+    </div>
+    <!--PAGINATION-->
+    <div class="pagination">
+        <span 
+            @click="goToSlide(index)"
+            v-for="(slide, index) in getSlideCount" 
+            :key="index" 
+            :class="{active : index + 1 === currentSlide}">
+
+        </span>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import { ref , onMounted, inject } from "vue"
+export default {
+    setup(){
+        let autoplayInterval;
+        const currentSlide = inject("currentSlide");
+        const getSlideCount = ref(null);
+        const autoPlayEnabled = ref(false);
+        const timeoutDuration = ref(15000);
+
+        //next slide
+        const nextSlide = () => {
+            if(currentSlide.value === getSlideCount.value){
+                currentSlide.value = 1;
+                return
+            }
+            currentSlide.value += 1;
+            restartAutoplay();
+        }
+
+        //prev slide
+        const prevSlide = () => {
+            if(currentSlide.value === 1){
+                currentSlide.value = getSlideCount.value; //OU FICAR NO PRIMEIRO SLIDE
+                return
+            }
+            currentSlide.value -=1;
+            restartAutoplay();
+        }
+
+        const goToSlide = (index) => {
+            currentSlide.value = index + 1;
+            restartAutoplay();
+        }
+
+        // autoplay
+        const autoPlay = () => {
+            autoplayInterval = setInterval(() =>{
+                nextSlide()
+            }, timeoutDuration.value);
+        }
+        const restartAutoplay = () => {
+            clearInterval(autoplayInterval);
+                if (autoPlayEnabled.value) {
+                    autoPlay();
+            }
+        };
+        if (autoPlayEnabled.value) {
+            autoPlay();
+        }
+        
+
+        onMounted(() =>{
+            getSlideCount.value = document.querySelectorAll(".slide").length; //COMENTARARARAR
+            //console.log(getSlideCount.value);
+        })
+
+        return { currentSlide , nextSlide, prevSlide , getSlideCount, goToSlide};
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .navigation {
+        padding: 0 16px;
+        height: 100%;
+        width: 100%;
+        position: absolute;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        .toggle-page {
+            display: flex;
+            flex: 1;
+        }
+        .right{
+            justify-content: flex-end;
+        }
+
+        i{
+            cursor: pointer;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            border-radius: 50%;
+            width: 40px;
+            height: 40px;
+            background-color:#9a8ec5 ;
+            color: #ffffff;
+        }
+    }
+    .pagination{
+        position: absolute;
+        bottom: 24px;
+        width: 100%;
+        display: flex;
+        gap:16px;
+        justify-content: center;
+        align-items: center;
+
+        span{
+            cursor: pointer;
+            width: 20px;
+            height: 20px;
+            border-radius: 50%;
+            background-color: #0000;
+            box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+        }
+        .active{
+            background-color: #cfcdd4;
+        }
+    }
+</style>
